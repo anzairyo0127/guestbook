d0a6a1b43206c6c649de520cfb75febc08ab2ff4
anzairyo0127 <36684885+anzairyo0127@users.noreply.github.com>
2019-10-09 23:40:40 +0900

    add heroku

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,6 +2,8 @@ import {createApp} from './factory'
 
 const app = createApp();
 
-app.listen(8888, ()=>{
-    console.log('Listen to port http://localhost:8888');
+const port = process.env.PORT || 5000;
+
+app.listen(port, ()=>{
+    console.log(`Listen to port http://localhost:${port}`);
 })
