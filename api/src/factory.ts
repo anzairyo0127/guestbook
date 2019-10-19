a143faa7b061b2bdcb6228258a53884c728e8b98
anzairyo0127 <36684885+anzairyo0127@users.noreply.github.com>
2019-10-19 16:40:18 +0900

    cors修正

--- api/src/factory.ts
+++ api/src/factory.ts
@@ -1,5 +1,6 @@
 import e from "express";
 import * as bodyParser from "body-parser";
+import cors from "cors";
 
 import {create_posts} from "./singletons"
 import postsRouter from "./controllers/posts"
@@ -9,6 +10,7 @@ export function createApp(){
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
     app.use('/', postsRouter);
+    app.use(cors);
     create_posts(<string>process.env.DATABASE_URL);
     return app
 };
