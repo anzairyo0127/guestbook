68170d01f5829489733621d6d4eee6ee25826025
anzairyo0127 <36684885+anzairyo0127@users.noreply.github.com>
2019-10-19 12:29:31 +0900

    WIP)バリデーション

---
+++ api/src/Models/Val.ts
@@ -0,0 +1,8 @@
+import {postColumn} from "./Posts";
+
+export function postvalidate (insertData:postColumn){
+    const insertDataReg = new RegExp("\s()");
+    for (let data in insertData){
+        data 
+    }
+}
